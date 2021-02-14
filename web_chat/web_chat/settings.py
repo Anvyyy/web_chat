ff9e87e20e80f38899cebd2958fe93ad4e39cc18
Дмитрий <anvyyy1@gmail.com>
2021-02-15 01:23:50 +0300

    version 1

--- web_chat/web_chat/settings.py
+++ web_chat/web_chat/settings.py
@@ -77,14 +77,22 @@ WSGI_APPLICATION = 'web_chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'web_chat',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres0987',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'web_chat',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres0987',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_sqlite3'),
+
     }
 }
 
