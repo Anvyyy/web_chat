02df6e4a2710f9173845dc53eb2a2d32ca442fd0
Дмитрий <anvyyy1@gmail.com>
2021-02-15 01:02:53 +0300

    version 1

--- web_chat/web_chat/settings.py
+++ web_chat/web_chat/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,8 +138,7 @@ CHANNEL_LAYERS = {
 
 }
 
-ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
