0d49070e00dd4f0bd2a5b6897a54b8fcab842f92
Дмитрий <anvyyy1@gmail.com>
2021-02-15 21:32:13 +0300

    version 1

--- web_chat/web_chat/settings.py
+++ web_chat/web_chat/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'web_chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'web_chat',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres0987',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_sqlite3'),
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'web_chat',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres0987',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db_sqlite3'),
+#
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
